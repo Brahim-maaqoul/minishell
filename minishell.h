1c95ac25ad3967dd6a6fbf46bee961f10472a508
Brahim  Maaqoul <wacakoule@gmail.com>
2022-07-16 18:20:43 +0100

    starting

---
+++ minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmaaqoul <bmaaqoul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/16 18:14:50 by bmaaqoul          #+#    #+#             */
+/*   Updated: 2022/07/16 18:17:21 by bmaaqoul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+# endif
