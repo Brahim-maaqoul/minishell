a73f67bc8895c1522f5b65c2bbcf7df128d4b99b
MITIRIX <ghoufal1337@gmail.com>
2022-09-27 01:36:16 +0100

    minishell

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -3,24 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bmaaqoul <bmaaqoul@student.42.fr>          +#+  +:+       +#+        */
+/*   By: orekabe <orekabe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 23:03:15 by orekabe           #+#    #+#             */
-/*   Updated: 2022/09/05 00:21:47 by bmaaqoul         ###   ########.fr       */
+/*   Updated: 2022/09/26 21:26:15 by orekabe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
-
-// int	get_rand(char *str)
-// {
-// 	if (!ft_strncmp(str, "&&", 2) && !ft_strncmp(str, "||", 2)
-// 		&& !ft_strncmp(str, "|", 1) && !ft_strncmp(str, "<", 1)
-// 			&& !ft_strncmp(str, ">", 1) && !ft_strncmp(str, "<<", 2)
-// 				&& !ft_strncmp(str, " ", 1))
-// 		return (0);
-// 	return (1);
-// }
 
 int	get_token(char *str)
 {
@@ -30,16 +20,18 @@ int	get_token(char *str)
 		return (OR);
 	else if (!ft_strncmp(str, "|", 1))
 		return (PIPE);
-	else if (!ft_strncmp(str, "<", 1))
-		return (L_RD);
-	else if (!ft_strncmp(str, ">", 1))
-		return (R_RD);
 	else if (!ft_strncmp(str, "<<", 2))
 		return (DL_RD);
 	else if (!ft_strncmp(str, ">>", 2))
 		return (DR_RD);
+	else if (!ft_strncmp(str, "<", 1))
+		return (L_RD);
+	else if (!ft_strncmp(str, ">", 1))
+		return (R_RD);
 	else if (!ft_strncmp(str, " ", 1))
 		return (SP);
+	else if (!ft_strncmp(str, "(", 1))
+		return (P);
 	return (WORD);
 }
 
@@ -52,57 +44,72 @@ int	check_q(char c, int flag)
 	return (flag);
 }
 
-t_list	*tokenizer(t_list *lst, char *n_cmd)
+int	get_p_lvl(char *n_cmd)
 {
-	t_list	*tmp;
-	char	*str;
-	int		i;
-	int		j;
-	int		b;
-	int		q;
+	int	i;
+	int	p;
+	int	lvl;
 
 	i = 0;
-	j = 0;
-	b = 0;
-	q = 0;
-	tmp = lst;
-	while (n_cmd[i])
+	p = 1;
+	lvl = 1;
+	while (n_cmd[i] && p > 0)
 	{
-		q = check_q(n_cmd[i], q);
-		if (n_cmd[i] == ' ' && !q)
+		if (n_cmd[i] == '(')
 		{
-			str = ft_strndup(n_cmd + j, i - j);
-			tmp = ft_lstnew(str, get_token(str));
-			ft_lstadd_back(&lst, tmp);
-			tmp = tmp->next;
-			j = i + 1;
-			b = 1;
+			p++;
+			lvl++;
 		}
-		if (b && n_cmd[i + 1])
-		{
-			tmp = ft_lstnew(ft_strndup(" ", 1), SP);
-			ft_lstadd_back(&lst, tmp);
-			tmp = tmp->next;
-			b = 0;
-		}
+		if (n_cmd[i] == ')')
+			p--;
 		i++;
 	}
-	str = ft_strndup(n_cmd + j, i - j);
-	tmp = ft_lstnew(str, get_token(str));
-	ft_lstadd_back(&lst, tmp);
-	tmp = tmp->next;
-	return (lst);
+	return (i);
+}
+
+t_list	*tokenizer(t_list *lst, char *n_cmd, t_lx_var *lx_var)
+{
+	while (n_cmd[lx_var->i])
+	{
+		lx_var->q = check_q(n_cmd[lx_var->i], lx_var->q);
+		if (n_cmd[lx_var->i] == '(')
+			lx_var->i += get_p_lvl(&n_cmd[lx_var->i + 1]);
+		if (n_cmd[lx_var->i] == ' ' && !lx_var->q)
+		{
+			lx_var->str = ft_strndup(n_cmd + lx_var->j, lx_var->i - lx_var->j);
+			lx_var->tmp = ft_lstnew(lx_var->str, get_token(lx_var->str));
+			ft_lstadd_back(&lst, lx_var->tmp);
+			lx_var->tmp = lx_var->tmp->next;
+			lx_var->j = lx_var->i + 1;
+		}
+		lx_var->i++;
+	}
+	lx_var->str = ft_strndup(n_cmd + lx_var->j, lx_var->i - lx_var->j);
+	lx_var->tmp = ft_lstnew(lx_var->str, get_token(lx_var->str));
+	ft_lstadd_back(&lst, lx_var->tmp);
+	return (lx_var->tmp);
 }
 
 t_list	*lexer(char *cmd, char *n_cmd, t_list *lst)
 {
+	t_lx_var	lx_var;
+
+	lx_var.tmp = lst;
+	lx_var.str = NULL;
+	lx_var.i = 0;
+	lx_var.j = 0;
+	lx_var.q = 0;
 	n_cmd = ft_calloc(ft_strlen(cmd) * 2, sizeof(char));
 	n_cmd = ft_strncpy(cmd, n_cmd, ft_strlen(cmd));
 	add_need_sp(cmd, n_cmd);
 	rm_extra_sp(n_cmd);
 	n_cmd = get_clean_cmd(n_cmd);
-	if (!syntax_analyser(n_cmd))
-			return (0);
-	lst = tokenizer(lst, n_cmd);
+	if (!syntax_analyser(n_cmd) || !check_valid_p(n_cmd))
+	{
+		free(n_cmd);
+		return (NULL);
+	}
+	lst = tokenizer(lst, n_cmd, &lx_var);
+	free(n_cmd);
 	return (lst);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: orekabe <orekabe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 02:37:13 by orekabe           #+#    #+#             */
-/*   Updated: 2022/08/25 02:15:20 by orekabe          ###   ########.fr       */
+/*   Updated: 2022/09/24 01:16:48 by orekabe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ typedef struct s_list
 {
 	void			*content;
 	int				token;
+	struct s_list	*prev;
 	struct s_list	*next;
 }				t_list;
 
@@ -30,9 +31,11 @@ void	*ft_calloc(int count, int size);
 char	*ft_strndup(char *s, int size);
 int		ft_strncmp(char *s1, char *s2, int n);
 char	*ft_strncpy(char *src, char *dst, int size);
+char	*ft_strjoin(char *s1, char *s2);
 void	*ft_memcpy(void *dst, const void *src, int n);
 void	*ft_memmove(void *dst, const void *src, int len);
 t_list	*ft_lstnew(void *content, int token);
 void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstclear(t_list *lst);
+
 #endif
